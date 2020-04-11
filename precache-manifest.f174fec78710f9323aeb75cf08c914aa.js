12245508eed7f0559dbe0831d8488d6ae90d0eb4
abbadmus <abdulbadmus75@gmail.com>
2020-04-11 08:25:59 +0100

    Updates

---
+++ precache-manifest.f174fec78710f9323aeb75cf08c914aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d62723aef43ed62b7dc8b7be0b0d6fe",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d7f3f00b10a9bbe44912",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7566b6e63c6d40e2baf4",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d7f3f00b10a9bbe44912",
+    "url": "/contactmanager/static/js/2.80f57e85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.80f57e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7566b6e63c6d40e2baf4",
+    "url": "/contactmanager/static/js/main.783ea0cd.chunk.js"
+  },
+  {
+    "revision": "98d49c782551fbed3aca",
+    "url": "/contactmanager/static/js/runtime-main.53501af3.js"
+  }
+]);
